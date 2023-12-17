58a73e10f5eaabac29dfefdff05d63ca89be39ba
Nikiaker <md.macek@gmail.com>
2023-12-17 18:52:18 +0100

    Ulepszony Api

--- src/main/java/org/sortingmadness/SortDataModel.java
+++ src/main/java/org/sortingmadness/SortDataModel.java
@@ -1,21 +1,45 @@
 package org.sortingmadness;
 
+import jakarta.annotation.Nullable;
+
 public class SortDataModel<T> {
     private T data;
+    private String sortType;
+    private Boolean descending = false;
+    private Integer iterations = 0;
 
     public SortDataModel() {
 
     }
 
-    public SortDataModel(T data) {
+    public SortDataModel(T data, String sortType, @Nullable Boolean descending, @Nullable Integer iterations) {
         this.data = data;
+        this.sortType = sortType;
+        if (descending != null) {
+            this.descending = descending;
+        }
+        if (iterations != null) {
+            this.iterations = iterations;
+        }
     }
 
-    public T getData() {
+    public T GetData() {
         return this.data;
     }
 
-    public void setData(T data) {
+    public String GetSortType() {
+        return this.sortType;
+    }
+
+    public Boolean IsDescending() {
+        return this.descending;
+    }
+
+    public Integer Iterations() {
+        return this.iterations;
+    }
+
+    public void SetData(T data) {
         this.data = data;
     }
 }
