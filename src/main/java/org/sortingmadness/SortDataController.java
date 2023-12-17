a70aac1db61cb64040bfd00bf668c411efc9dd5a
Nikiaker <md.macek@gmail.com>
2023-12-17 20:33:59 +0100

    Revert "Ulepszony Api"
    This reverts commit 58a73e10f5eaabac29dfefdff05d63ca89be39ba.

--- src/main/java/org/sortingmadness/SortDataController.java
+++ src/main/java/org/sortingmadness/SortDataController.java
@@ -2,43 +2,17 @@ package org.sortingmadness;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
 
 @RestController
 public class SortDataController {
     @PostMapping("/sort_int")
     public SortDataModel<Integer[]> sortIntData(@RequestBody SortDataModel<Integer[]> dataModel) {
-        try {
-            Integer[] data = dataModel.GetData();
-            SortingStrategyEnum sortType = this.GetSortType(dataModel.GetSortType());
-
-            Sorter sorter = new Sorter();
-            sorter.setSortingStrategy(sortType);
-            sorter.getAlgorithm().sort(data);
-
-            dataModel.SetData(data);
-            return dataModel;
-        } catch (Exception e) {
-            dataModel.SetData(new Integer[]{0});
-            return dataModel;
-        }
-    }
-
-    @PostMapping("/sort_double")
-    public SortDataModel<Double[]> sortDoubleData(@RequestBody SortDataModel<Double[]> dataModel) {
-
-    }
-
-    @PostMapping("/sort_string")
-    public SortDataModel<String> sortStringData(@RequestBody SortDataModel<String> dataModel) {
-
-    }
-
-    private SortingStrategyEnum GetSortType(String sortType) throws Exception {
-        return switch (sortType) {
-            case "QUICK_SORT" -> SortingStrategyEnum.QUICK_SORT;
-            case "MERGE_SORT" -> SortingStrategyEnum.MERGE_SORT;
-            case "BUBBLE_SORT" -> SortingStrategyEnum.BUBBLE_SORT;
-            default -> throw new Exception("Nieznany typ sortowania");
-        };
+        Integer[] data = dataModel.getData();
+        Sorter sorter = new Sorter();
+        sorter.setSortingStrategy(SortingStrategyEnum.BUBBLE_SORT);
+        sorter.getAlgorithm().sort(data);
+        dataModel.setData(data);
+        return dataModel;
     }
 }
--- src/main/java/org/sortingmadness/SortDataModel.java
+++ src/main/java/org/sortingmadness/SortDataModel.java
@@ -1,45 +1,21 @@
 package org.sortingmadness;
 
-import jakarta.annotation.Nullable;
-
 public class SortDataModel<T> {
     private T data;
-    private String sortType;
-    private Boolean descending = false;
-    private Integer iterations = 0;
 
     public SortDataModel() {
 
     }
 
-    public SortDataModel(T data, String sortType, @Nullable Boolean descending, @Nullable Integer iterations) {
+    public SortDataModel(T data) {
         this.data = data;
-        this.sortType = sortType;
-        if (descending != null) {
-            this.descending = descending;
-        }
-        if (iterations != null) {
-            this.iterations = iterations;
-        }
     }
 
-    public T GetData() {
+    public T getData() {
         return this.data;
     }
 
-    public String GetSortType() {
-        return this.sortType;
-    }
-
-    public Boolean IsDescending() {
-        return this.descending;
-    }
-
-    public Integer Iterations() {
-        return this.iterations;
-    }
-
-    public void SetData(T data) {
+    public void setData(T data) {
         this.data = data;
     }
 }
