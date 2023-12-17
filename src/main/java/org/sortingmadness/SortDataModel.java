af4eb6e0575930cb65fc5989c07e3a65533d4417
Nikiaker <md.macek@gmail.com>
2023-12-17 20:56:05 +0100

    Update SortDataModel.java

--- src/main/java/org/sortingmadness/SortDataModel.java
+++ src/main/java/org/sortingmadness/SortDataModel.java
@@ -1,16 +1,26 @@
 package org.sortingmadness;
 
+import jakarta.annotation.Nullable;
+
 public class SortDataModel<T> {
     private T data;
     private String sortType;
+    private Boolean descending = false;
+    private Integer iterations = 0;
 
     public SortDataModel() {
 
     }
 
-    public SortDataModel(T data, String sortType) {
+    public SortDataModel(T data, String sortType, @Nullable Boolean descending, @Nullable Integer iterations) {
         this.data = data;
         this.sortType = sortType;
+        if (descending != null) {
+            this.descending = descending;
+        }
+        if (iterations != null) {
+            this.iterations = iterations;
+        }
     }
 
     public T getData() {
@@ -27,5 +37,21 @@ public class SortDataModel<T> {
 
     public void setSortType(String sortType) {
         this.sortType = sortType;
+    }
+
+    public Boolean IsDescending() {
+        return this.descending;
+    }
+
+    public void setDescending(Boolean descending) {
+        this.descending = descending;
+    }
+
+    public Integer getIterations() {
+        return this.iterations;
+    }
+
+    public void setIterations(Integer iterations) {
+        this.iterations = iterations;
     }
 }
